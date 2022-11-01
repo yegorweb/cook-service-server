794ff761fbe7ab19f2573b6ef47a8191e335ef32
Yegor <egorknyazev10@gmail.com>
2022-11-01 19:33:03 +0400

    added new functions

--- server.js
+++ server.js
@@ -65,6 +65,57 @@ app.post('/registration', async function(req, res) {
         res.status(400).json({message: 'Ошибка сервера: ' + e})
     }
 })
+app.post('/add-to-cart', async function(req, res) {
+    try {
+        let value = req.body
+        var id = new ObjectId((value.user_id).trim())
+        let user = await users.findOne({'_id': id})
+        console.log(value)
+        if (user) {
+            users.updateOne({'_id': id}, {$push: {'cart': value.item}})
+            res.status(200).send()
+        } else {
+            res.status(404).send()
+        }
+    } catch(e) {
+        res.send({message: 'Error: ' + e})
+        console.error('Error: ' + e)
+    }
+})
+app.get('/items', async function(req, res) {
+    try {
+        let result = await db.collection('items').find({visible: true}).toArray()
+        result ? res.status(200).send(result) : res.status(404).send()
+        return
+    } catch(e) {
+        res.send({message: 'Error: ' + e})
+        console.error('Error: ' + e)
+    }
+})
+app.get('/user-orders', async function(req, res) {
+    try {
+        let id = new ObjectId((req.query.id).trim())
+        var user = await users.findOne({'_id': id})
+        var user_orders = []
+        if (user) {
+            for (let i = 0; i < user.orders.length; i++) {
+                let order_id = new ObjectId((user.orders[i]).trim())
+                let order = await orders.findOne({'_id': order_id})
+                console.log(order)
+                user_orders.push(order)
+            }
+        } else {
+            res.status(404).send()
+            return
+        }
+        console.log('ORDERS: '+user_orders)
+        res.send(user_orders)
+        return
+    } catch(e) {
+        res.send({message: 'Error: ' + e})
+        console.error('Error: ' + e)
+    }
+})
 app.get('/order', async function(req, res) {
     try {
         let id = new ObjectId((req.query.id).trim())
@@ -72,6 +123,38 @@ app.get('/order', async function(req, res) {
         let order = await orders.findOne({'_id': id})
         console.log(order)
         res.send(order)
+    } catch(e) {
+        res.send({message: 'Error: ' + e})
+        console.error('Error: ' + e)
+    }
+})
+app.get('/get-cart', async function(req, res) {
+    try {
+        let id = new ObjectId((req.query.id).trim())
+        var user = await users.findOne({'_id': id})
+        if (!user) {
+            res.status(404).send()
+            return
+        }
+        var cart = user.cart
+        if (cart.length == 0) {
+            res.send()
+            return
+        }
+        for (let i = 0; i < cart.length; i++) {
+            let item_id = new ObjectId((cart[i]._id).trim())
+            let item = await items.findOne({'_id': item_id})
+            if (item) {
+                cart[i] = Object.assign({}, item, cart[i])
+                console.log(Object.assign({}, item, cart[i]))
+            } else {
+                res.status(404).send()
+                return
+            }
+        }
+        res.send(cart)
+        console.log(cart)
+        return
     } catch(e) {
         res.send({message: 'Error: ' + e})
         console.error('Error: ' + e)
