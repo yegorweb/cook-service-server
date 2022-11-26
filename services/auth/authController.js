24a249573398ffce27d99ed9a54bf3358a3c894c
Yegor <egorknyazev10@gmail.com>
2022-11-26 20:48:59 +0400

    tssss... don't store secrets in a repository

--- services/auth/authController.js
+++ services/auth/authController.js
@@ -1,9 +1,8 @@
-const User = require('./models/User')
-const Role = require('./models/Role')
+const Role = require('../../models/Role')
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const {secret} = require("./config")
-const server = require('./server')
+const {secret} = require("../../config")
+const server = require('../../server')
 
 const generateAccessToken = (id, roles) => {
     const payload = {
@@ -17,56 +16,33 @@ class authController {
     async registration(req, res) {
         try {
             console.log('Пытались зарегаться')
-            console.log()
-            const {name, fullphone, phone, password} = req;
-            const candidate = await User.findOne({phone})
+            const {name, fullphone, phone, password} = req.body;
+            let candidate = await users.findOne({phone: phone})
             if (candidate) {
-                return res.status(400).json({message: "Пользователь с таким же номером телефона уже существует"})
+                return res.status(400).json({message: "Пользователь с таким же номером телефона уже существует", type: 'error'})
             }
             const hashPassword = bcrypt.hashSync(password, 7);
             console.log(req.body)
             const user = new User({name: name, fullphone: fullphone, phone: phone, password: hashPassword, roles: ["USER"]})
-            server.users.insertOne(value, (err, result) => {
+            server.users.insertOne(user, (err, result) => {
                 if (err) {
                     console.error(err)
                     console.log("не добавлен(((\n-------------------")
-                    res.status(500).json({message: err })
+                    res.status(500).json({message: 'Ошибка сервера: ' + err })
                     res.send()
                     return
                 }
                 console.log(result)
                 console.log("добавлен)\n-------------------")
-                res.status(200).json({message: "Пользователь успешно зарегистрирован"})
-                res.send()
-                return
+                res.status(200)
+                res.send({ok: true, message: 'Вы успешно зарегистрированы', accessToken: '', type: 'success', user: user})
             })
-            // let value = req.body
-            // console.log(value)
-            // console.log("добавляем чела")
-            // if (!users.findOne({phone: value.phone})) {
-            //     users.insertOne(value, (err, result) => {
-            //         if (err) {
-            //             console.error(err)
-            //             console.log("не добавлен(((\n-------------------")
-            //             res.status(500).json({ err: err })
-            //             res.send()
-            //             return
-            //         }
-            //         console.log(result)
-            //         console.log("добавлен)\n-------------------")
-            //         res.status(200).json({ ok: true })
-            //         res.send()
-            //         return
-            //     })
-            // } else {
-            //     res.status(401).json({ err: 'Пользователь с таким же номером телефона уже существует' })
-            //     console.log('Пытался войти чел с существующим номером телефонв')
-            //     res.send()
-            //     return
-            // }
         } catch (e) {
-            res.status(400).json({message: 'Registration error' + e})
+            res.status(400).json({message: 'Ошибка сервера: ' + e, type: 'error'})
         }
+    }
+    async login(req, res) {
+
     }
 }
 
